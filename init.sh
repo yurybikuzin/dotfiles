5b693b6b7dccec14b83a59563080c0d3276582da
Yury Bikuzin <yury.bikuzin@gmail.com>
2020-07-17 17:28:06 +0300

    bash_profile => bashrc

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
 
 # shellcheck disable=SC2016
-echo 'source $HOME/dotfiles/bash_profile' >> ~/.bash_profile
+echo 'source $HOME/dotfiles/bashrc' >> ~/.bashrc
 
