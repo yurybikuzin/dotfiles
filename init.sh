23f51a832166d52ba90985d27d4e36e445d1d9b1
Yury Bikuzin <yury.bikuzin@gmail.com>
2020-07-17 16:46:48 +0300

    some fixes

--- init.sh
+++ init.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-ln ~/dotfiles/tmux/conf ~/.tmux.conf
-ln ~/dotfiles/tmux/conf.linux ~/.tmux.conf.linux
-ln ~/dotfiles/tmux/conf.osx ~/.tmux.conf.osx
-ln ~/dotfiles/tmux/conf.cygwin ~/.tmux.conf.cygwin
-
-ln ~/dotfiles/vimrc ~/.vimrc
-ln ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/tmux/conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
 
 # shellcheck disable=SC2016
 echo 'source $HOME/dotfiles/bash_profile' >> ~/.bash_profile
